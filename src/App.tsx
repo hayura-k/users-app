dafd084476a6bbb764dd29502d8944eafa917be8
yuki hara <hara.0509@icloud.com>
2022-04-19 00:09:04 +0900

    取得してきたusersに型定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type Users = {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    }
+  }
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
+}[]
+
+
+const baseUrl: string = 'https://jsonplaceholder.typicode.com/users'
 
 function App() {
+  const [users, setUsers] = useState<Users>([]);
+
+  console.log(users)
+
+  useEffect(() => {
+    axios.get(baseUrl).then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {users.map((user) => (
+          <li>
+            {user.id}: {user.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
